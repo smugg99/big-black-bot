5b4a9fa4b8542f9acc8bd1d982a3b306a5b61bce
Dudus Jestem <dudak799@gmail.com>
2024-01-28 23:46:35 +0100

    added nsfw channels filter

--- src/main.py
+++ src/main.py
@@ -10,46 +10,49 @@ bot = Client(intents=Intents.DEFAULT)
 base_url = "https://blackcockpictures.com/category/black-cock-pictures/"
 
 async def extract_image_sources(url):
-    async with async_playwright() as p:
-        browser = await p.chromium.launch()
-        context = await browser.new_context()
-        page = await context.new_page()
+	async with async_playwright() as p:
+		browser = await p.chromium.launch()
+		context = await browser.new_context()
+		page = await context.new_page()
 
-        await page.goto(url)
-        await page.wait_for_selector(".gallerywp-grid-post")
+		await page.goto(url)
+		await page.wait_for_selector(".gallerywp-grid-post")
 
-        html_content = await page.content()
+		html_content = await page.content()
 
-    soup = BeautifulSoup(html_content, "html.parser")
+	soup = BeautifulSoup(html_content, "html.parser")
 
-    post_elements = soup.find_all("div", class_="gallerywp-grid-post")
-    image_sources = [post.select_one(".gallerywp-grid-post-thumbnail-img")["src"] for post in post_elements
-                     if post.select_one(".gallerywp-grid-post-thumbnail-img")]
+	post_elements = soup.find_all("div", class_="gallerywp-grid-post")
+	image_sources = [post.select_one(".gallerywp-grid-post-thumbnail-img")["src"] for post in post_elements
+					 if post.select_one(".gallerywp-grid-post-thumbnail-img")]
 
-    return image_sources
+	return image_sources
 
 @slash_command(name="givemeanigga", description="Gives you a nigga :)")
 async def give_me_a_nigga(ctx: SlashContext):
-    random_page_number = random.randint(1, 100)
-    image_sources = await extract_image_sources(base_url + f"page/{random_page_number}")
+	if ctx.channel.nsfw:
+		random_page_number = random.randint(1, 100)
+		image_sources = await extract_image_sources(base_url + f"page/{random_page_number}")
 
-    embed = Embed(
-        title="Look at that gyyaat nigga",
-        description="",
-        color=0
-    )
-    
-    embed.set_image(url=random.choice(image_sources))
+		embed = Embed(
+			title="Look at that gyyaat nigga 😛😛😛",
+			description="",
+			color=0
+		)
+		
+		embed.set_image(url=random.choice(image_sources))
 
-    await ctx.send(embed=embed)
+		await ctx.send(embed=embed)
+	else:
+		await ctx.send("This command can only be used in NSFW channels nigga 💀💀💀")
 
 @listen()
 async def on_ready():
-    print("Logged in")
+	print("Logged in")
 
 def main():
-    load_dotenv(".env")
-    bot.start(os.getenv("BOT_TOKEN"))
+	load_dotenv(".env")
+	bot.start(os.getenv("BOT_TOKEN"))
 
 if __name__ == "__main__":
-    main()
+	main()
